59456b726082ae027dfe7b5a92ae3e73a948b8f4
johnnydoni <inods@hotmail.com>
2018-04-09 16:56:31 -0300

    Inserir e alterar

--- index.php
+++ index.php
@@ -20,8 +20,21 @@ require_once("config.php");
 //echo json_encode($search);
 
 // Carregar um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("sdjr", "123456");
+//echo $usuario;
+
+// Criando um novo usuário
+//$aluno = new Usuario();
+//$aluno->setDeslogin("aluno");
+//$aluno->setDessenha("@lun0");
+//$aluno->insert();
+//echo $aluno;
+
 $usuario = new Usuario();
-$usuario->login("sdjr", "123456");
+$usuario->loadById(7);
+$usuario->update("professor", "!@#$");
 echo $usuario;
+
 
  ?>
